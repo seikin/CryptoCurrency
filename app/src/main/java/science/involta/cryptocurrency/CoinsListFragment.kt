74eca8ed72c44ac99b742c6a452be8d57ec5938b
seiki <seikin.alexey@gmail.com>
2021-11-23 16:39:17 +0300

    Initial commit

---
+++ app/src/main/java/science/involta/cryptocurrency/CoinsListFragment.kt
@@ -0,0 +1,15 @@
+package science.involta.cryptocurrency
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CoinsListFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_coins_list, container, false)
+    }
+
+}
---
+++ app/src/main/java/science/involta/cryptocurrency/Favotites.kt
@@ -0,0 +1,13 @@
+package science.involta.cryptocurrency
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FavotitesFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+}
